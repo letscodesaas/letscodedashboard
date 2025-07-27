74f41b5dce2b96ae5bd3ee82179e7e4823b7199a
Rohit kumar yadav <rohitkuyada@gmail.com>
2025-07-27 14:55:35 +0530

    na1

---
+++ src/app/dashboard/interview-experience/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1>Interview Experience</h1>
+      <p>Share your interview experiences and tips.</p>
+    </div>
+  )
+}
+
+export default page
