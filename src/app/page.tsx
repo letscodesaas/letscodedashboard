17b7c2573aec3cf924ce3db4a8fecee8a7fd618e
Rohit kumar yadav <rohitkuyada@gmail.com>
2025-07-27 14:51:31 +0530

    na

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,10 @@ export default function LoginPage() {
       <div className="grid min-h-svh lg:grid-cols-2">
         <div className="flex flex-col gap-4 p-6 md:p-10">
           <div className="flex justify-center gap-2 md:justify-start">
-              <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-                <GalleryVerticalEnd className="size-4" />
-              </div>
-              Let&apos;s Code
+            <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+              <GalleryVerticalEnd className="size-4" />
+            </div>
+            Let&apos;s Code
           </div>
           <div className="flex flex-1 items-center justify-center">
             <div className="w-full max-w-xs">
