a0088295c0d721792a6050ff7109bed6cb18b39c
shu12388y <spl.sp999@gmail.com>
2026-01-07 15:16:54 +0530

    added total registation and filter

--- src/app/dashboard/contest/page.tsx
+++ src/app/dashboard/contest/page.tsx
@@ -1,28 +1,7 @@
-// import React from 'react';
-// import { columns, Contest } from './_components/columns';
-// import { DataTable } from './_components/data-table';
-// import { trpc } from '@/app/_trpc/client';
-
-// async function getData(): Promise<Contest[]> {
-//   const info = await trpc.contest.getregisations.query();
-//   return info;
-// }
-
-// async function Page() {
-//   const data = await getData();
-//   return (
-//     <div>
-//       <DataTable columns={columns} data={data} />
-//     </div>
-//   );
-// }
-
-// export default Page;
-
 'use client';
 import React, { useState, useEffect } from 'react';
 import { columns } from './_components/columns';
-import { DataTable } from './_components//data-table';
+import { DataTable } from './_components/data-table';
 import { trpc } from '@/app/_trpc/client';
 
 function Page() {
@@ -40,6 +19,9 @@ function Page() {
 
   return (
     <div>
+      <div className='mb-4'>
+        <div>Total Regisation : {data.length}</div>
+      </div>
       {loading ? 'Loading..' : <DataTable columns={columns} data={data} />}
     </div>
   );
--- src/server/router/contest.router.ts
+++ src/server/router/contest.router.ts
@@ -3,7 +3,9 @@ import { publicProcedure, router } from '../trpc';
 export const contestRouter = router({
   getregisations: publicProcedure.query(async (opts) => {
     try {
-      const info = await opts.ctx.db.ContestRegistation.find({});
+      const info = await opts.ctx.db.ContestRegistation.find().sort({
+        createdAt:-1
+      });
       return info;
     } catch (error) {
       throw new Error(String(error));
