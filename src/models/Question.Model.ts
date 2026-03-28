42fb20f6f2d9ea84fb25e67665c3e23530aa2c37
shu12388y <spl.sp999@gmail.com>
2026-03-28 18:10:07 +0530

    added upload video section

--- src/models/Question.Model.ts
+++ src/models/Question.Model.ts
@@ -35,9 +35,9 @@ const questionSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    editoralvideo:{
-      type:String
-    }
+    editoralvideo: {
+      type: String,
+    },
   },
   {
     timestamps: true,
