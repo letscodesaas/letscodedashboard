1a8cfc0dee0379e27b9317caeb7e1cbb8a2337bf
shu12388y <spl.sp999@gmail.com>
2026-05-01 17:52:09 +0530

    added network layer auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,27 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
+export const revalidate = 0
 export async function middleware(request: NextRequest) {
   try {
+    console.log('middleware run')
     const headers = await request.headers.get('AUTHORIZATION');
+    const xff = request.headers.get('x-forwarded-for');
+    const ip =
+      (xff && xff.split(',')[0].trim()) ||
+      request.headers.get('x-real-ip') ||
+      'unknown';
+      const userIPAllow = process.env.ALLOWIPS;
+      const IPS = [];
+      if(!Array.isArray(userIPAllow)){
+        userIPAllow.split(",").map((ele)=>{
+          IPS.push(ele)
+        })
+      }
+      if(!IPS.includes(ip)){
+        console.log('not allowed')
+      return NextResponse.redirect(new URL('/', request.url));
+      }
     if (!headers) {
       return NextResponse.redirect(new URL('/', request.url));
     }
@@ -14,5 +32,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: '/about/:path*',
+  matcher: '/dashboard/:path*',
 };
